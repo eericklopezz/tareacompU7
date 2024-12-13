2c0713acc4dd8b9c9dca505d30c12a2b680385bc
Erick Lopez <erickjavier881134@gmail.com>
2024-12-13 12:06:31 +0100

    Modificacion 2

--- Git_Erick_Lopez/src/PC.java
+++ Git_Erick_Lopez/src/PC.java
@@ -3,5 +3,6 @@ public class PC {
 	public static void main(String[] args) {
 		System.out.println("Alumno: Erick Lopez");
 		System.out.println("Modificiación desde Master");
+		System.out.println("Modificación desde Develop");
 	}
 }
